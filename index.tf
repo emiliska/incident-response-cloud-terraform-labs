92a77f0c62efa581c245f0178bc0480d890ccbbd
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-20 00:38:39 -0500

    Add configuration for WIN2K16A, WIN2K16b, and XERUS01

--- index.tf
+++ index.tf
@@ -105,13 +105,13 @@ resource "azurerm_storage_account" "mystorageaccount" {
     }
 }
 
-# Create virtual machine
+# Create virtual machine XERUS01
 resource "azurerm_virtual_machine" "myterraformvm" {
-    name                  = "VM_Beaver01"
+    name                  = "VM_XERUS01"
     location              = "northcentralus"
     resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
     network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
-    vm_size               = "Standard_DS1_v2"
+    vm_size               = "Standard_DS3_v2"
 
     storage_os_disk {
         name              = "myOsDisk"
@@ -128,7 +128,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
     }
 
     os_profile {
-        computer_name  = "BEAVER01"
+        computer_name  = "XERUS01"
         admin_username = "azureuser"
     }
 
@@ -138,6 +138,80 @@ resource "azurerm_virtual_machine" "myterraformvm" {
             path     = "/home/azureuser/.ssh/authorized_keys"
             key_data = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
         }
+    }
+
+    boot_diagnostics {
+        enabled = "true"
+        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+    }
+
+    tags {
+        environment = "IRLAB"
+    }
+}
+
+# Create virtual machine WIN2K16A
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "VM_WIN2K16A"
+    location              = "northcentralus"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "MicrosoftWindowsServer"
+        offer     = "WindowsServer"
+        sku       = "2016-Datacenter"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "WIN2K16A"
+        admin_username = "azureuser"
+    }
+
+    boot_diagnostics {
+        enabled = "true"
+        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+    }
+
+    tags {
+        environment = "IRLAB"
+    }
+}
+
+# Create virtual machine WIN2K16B
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "VM_WIN2K16A"
+    location              = "northcentralus"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "MicrosoftWindowsServer"
+        offer     = "WindowsServer"
+        sku       = "2016-Datacenter"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "WIN2K16B"
+        admin_username = "azureuser"
     }
 
     boot_diagnostics {
