5552f447399e74be9b6d539d5e1961d5eea4884c
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-20 17:53:56 -0500

    refactor code to troubleshoot errors

--- main.tf
+++ main.tf
@@ -1,3 +1,124 @@
+# Create a resource group
+resource "azurerm_resource_group" "IRLAB" {
+    name     = "production"
+    location = "North Central US"
+
+    tags {
+        environment = "IRLAB"
+    }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "networks" {
+    name                = "IRNET01"
+    address_space       = ["10.0.0.0/16"]
+    location            = "northcentralus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    depends_on = ["${azurerm_resource_group.IRLAB}"]
+
+    tags {
+        environment = "IRLAB"
+    }
+
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnets" {
+    name                 = "Subnet_IRLAB01"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.1.0/24"
+
+    depends_on = ["${azurerm_virtual_network.networks}"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "publicips" {
+    name                         = "PublicIP_IRLAB01"
+    location                     = "northcentralus"
+    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+    public_ip_address_allocation = "dynamic"
+
+    depends_on = ["${azurerm_resource_group.IRLAB}"]
+
+    tags {
+        environment = "IRLAB"
+    }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "netsecgroup" {
+    name                = "NetworkSecurityGroup_IRLAB01"
+    location            = "northcentralus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    depends_on = ["${azurerm_resource_group.IRLAB}"]
+
+    tags {
+        environment = "IRLAB"
+    }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "nics" {
+    name                      = "NIC001"
+    location                  = "northcentralus"
+    resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
+    network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
+
+    ip_configuration {
+        name                          = "IP001"
+        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        private_ip_address_allocation = "dynamic"
+        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+    }
+
+    depends_on = ["${azurerm_virtual_network.networks}"]
+
+    tags {
+        environment = "IRLAB"
+    }
+
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = "${azurerm_resource_group.myterraformgroup.name}"
+    }
+
+    byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = "${azurerm_resource_group.myterraformgroup.name}"
+    location                    = "northcentralus"
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags {
+        environment = "IRLAB"
+    }
+
+    depends_on = ["${azurerm_storage_account.mystorageaccount}"]
+}
+
 # Create virtual machine XERUS01
 resource "azurerm_virtual_machine" "myterraformvm" {
     name                  = "XERUS01"
