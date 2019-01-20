793f23b4ecffc6514d8160ec7500c488bb25fee0
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-19 23:55:57 -0500

    Update asset names across infrastructure.

--- index.tf
+++ index.tf
@@ -65,13 +65,13 @@ resource "azurerm_network_security_group" "myterraformnsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "myterraformnic" {
-    name                      = "NIC_IRLAB01"
+    name                      = "NIC001"
     location                  = "northcentralus"
     resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
     network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
 
     ip_configuration {
-        name                          = "NicConfiguration_IRLAB01"
+        name                          = "IP001"
         subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
         private_ip_address_allocation = "dynamic"
         public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
