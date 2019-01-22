c490695332df5129888beeb560162f2c60b5dc38
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-22 12:35:49 -0500

    resource group names cannot exceed 80 characters, incident_response has been shortened to IR for brevity

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "incident_response_group" {
+resource "azurerm_resource_group" "IR_group" {
     name     = "incidentResponseLab"
     location = "northcentralus"
 
@@ -9,11 +9,11 @@ resource "azurerm_resource_group" "incident_response_group" {
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "incident_response_network" {
+resource "azurerm_virtual_network" "IR_network" {
     name                = "Network01"
     address_space       = ["10.0.0.0/16"]
     location            = "northcentralus"
-    resource_group_name = "${azurerm_resource_group.incident_response_group.name}"
+    resource_group_name = "${azurerm_resource_group.IR_group.name}"
 
     tags {
         environment = "IRLab01"
@@ -21,18 +21,18 @@ resource "azurerm_virtual_network" "incident_response_network" {
 }
 
 # Create subnet
-resource "azurerm_subnet" "incident_response_subnet_01" {
+resource "azurerm_subnet" "IR_subnet_01" {
     name                 = "SUBNET01"
-    resource_group_name  = "${azurerm_resource_group.incident_response_group.name}"
-    virtual_network_name = "${azurerm_virtual_network.incident_response_network.name}"
+    resource_group_name  = "${azurerm_resource_group.IR_group.name}"
+    virtual_network_name = "${azurerm_virtual_network.IR_network.name}"
     address_prefix       = "10.0.1.0/24"
 }
 
 # Create public IPs
-resource "azurerm_public_ip" "incident_response_public_ip" {
+resource "azurerm_public_ip" "IR_public_ip" {
     name                         = "Public IP 01"
     location                     = "northcentralus"
-    resource_group_name          = "${azurerm_resource_group.incident_response_group.name}"
+    resource_group_name          = "${azurerm_resource_group.IR_group.name}"
     public_ip_address_allocation = "dynamic"
 
     tags {
@@ -41,10 +41,10 @@ resource "azurerm_public_ip" "incident_response_public_ip" {
 }
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "incident_response_nsg_01" {
+resource "azurerm_network_security_group" "IR_nsg_01" {
     name                = "Network Security Group 01"
     location            = "northcentralus"
-    resource_group_name = "${azurerm_resource_group.incident_response_group.name}"
+    resource_group_name = "${azurerm_resource_group.IR_group.name}"
 
     security_rule {
         name                       = "SSH"
@@ -64,17 +64,17 @@ resource "azurerm_network_security_group" "incident_response_nsg_01" {
 }
 
 # Create network interface
-resource "azurerm_network_interface" "incident_response_nic_01" {
+resource "azurerm_network_interface" "IR_nic_01" {
     name                      = "NIC01"
     location                  = "northcentralus"
-    resource_group_name       = "${azurerm_resource_group.incident_response_group.name}"
-    network_security_group_id = "${azurerm_network_security_group.incident_response_nsg_01.id}"
+    resource_group_name       = "${azurerm_resource_group.IR_group.name}"
+    network_security_group_id = "${azurerm_network_security_group.IR_nsg_01.id}"
 
     ip_configuration {
         name                          = "myNicConfigurationA"
-        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        subnet_id                     = "${azurerm_subnet.IR_subnet_01.id}"
         private_ip_address_allocation = "dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.incident_response_public_ip.id}"
+        public_ip_address_id          = "${azurerm_public_ip.IR_public_ip.id}"
     }
 
     tags {
@@ -86,16 +86,16 @@ resource "azurerm_network_interface" "incident_response_nic_01" {
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.incident_response_group.name}"
+        resource_group = "${azurerm_resource_group.IR_group.name}"
     }
 
     byte_length = 8
 }
 
 # Create storage account for boot diagnostics
-resource "azurerm_storage_account" "incident_response_storage_account_01" {
+resource "azurerm_storage_account" "IR_storage_account_01" {
     name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = "${azurerm_resource_group.incident_response_group.name}"
+    resource_group_name         = "${azurerm_resource_group.IR_group.name}"
     location                    = "northcentralus"
     account_tier                = "Standard"
     account_replication_type    = "LRS"
@@ -109,8 +109,8 @@ resource "azurerm_storage_account" "incident_response_storage_account_01" {
 resource "azurerm_virtual_machine" "myterraformvmDETMNG001" {
     name                  = "DETMNG001"
     location              = "northcentralus"
-    resource_group_name   = "${azurerm_resource_group.incident_response_group.name}"
-    network_interface_ids = ["${azurerm_network_interface.incident_response_nic_01.id}"]
+    resource_group_name   = "${azurerm_resource_group.IR_group.name}"
+    network_interface_ids = ["${azurerm_network_interface.IR_nic_01.id}"]
     vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
@@ -134,7 +134,7 @@ resource "azurerm_virtual_machine" "myterraformvmDETMNG001" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.incident_response_storage_account_01.primary_blob_endpoint}"
+        storage_uri = "${azurerm_storage_account.IR_storage_account_01.primary_blob_endpoint}"
     }
 
     tags {
@@ -147,8 +147,8 @@ resource "azurerm_virtual_machine" "myterraformvmDETMNG001" {
 resource "azurerm_virtual_machine" "myterraformvmDETMNG002" {
     name                  = "DETMNG002"
     location              = "northcentralus"
-    resource_group_name   = "${azurerm_resource_group.incident_response_group.name}"
-    network_interface_ids = ["${azurerm_network_interface.incident_response_nic_01.id}"]
+    resource_group_name   = "${azurerm_resource_group.IR_group.name}"
+    network_interface_ids = ["${azurerm_network_interface.IR_nic_01.id}"]
     vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
@@ -172,7 +172,7 @@ resource "azurerm_virtual_machine" "myterraformvmDETMNG002" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.incident_response_storage_account_01.primary_blob_endpoint}"
+        storage_uri = "${azurerm_storage_account.IR_storage_account_01.primary_blob_endpoint}"
     }
 
     tags {
@@ -184,8 +184,8 @@ resource "azurerm_virtual_machine" "myterraformvmDETMNG002" {
 resource "azurerm_virtual_machine" "myterraformvmXERUS01" {
     name                  = "XERUS01"
     location              = "northcentralus"
-    resource_group_name   = "${azurerm_resource_group.incident_response_group.name}"
-    network_interface_ids = ["${azurerm_network_interface.incident_response_nic_01.id}"]
+    resource_group_name   = "${azurerm_resource_group.IR_group.name}"
+    network_interface_ids = ["${azurerm_network_interface.IR_nic_01.id}"]
     vm_size               = "Standard_DS3_v2"
 
     storage_os_disk {
@@ -217,7 +217,7 @@ resource "azurerm_virtual_machine" "myterraformvmXERUS01" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.incident_response_storage_account_01.primary_blob_endpoint}"
+        storage_uri = "${azurerm_storage_account.IR_storage_account_01.primary_blob_endpoint}"
     }
 
     tags {
@@ -229,8 +229,8 @@ resource "azurerm_virtual_machine" "myterraformvmXERUS01" {
 resource "azurerm_virtual_machine" "myterraformvmXERUS02" {
     name                  = "XERUS02"
     location              = "northcentralus"
-    resource_group_name   = "${azurerm_resource_group.incident_response_group.name}"
-    network_interface_ids = ["${azurerm_network_interface.incident_response_nic_01.id}"]
+    resource_group_name   = "${azurerm_resource_group.IR_group.name}"
+    network_interface_ids = ["${azurerm_network_interface.IR_nic_01.id}"]
     vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
@@ -262,7 +262,7 @@ resource "azurerm_virtual_machine" "myterraformvmXERUS02" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.incident_response_storage_account_01.primary_blob_endpoint}"
+        storage_uri = "${azurerm_storage_account.IR_storage_account_01.primary_blob_endpoint}"
     }
 
     tags {
@@ -274,8 +274,8 @@ resource "azurerm_virtual_machine" "myterraformvmXERUS02" {
 resource "azurerm_virtual_machine" "myterraformvmWIN2K16A" {
     name                  = "WIN2K16A"
     location              = "northcentralus"
-    resource_group_name   = "${azurerm_resource_group.incident_response_group.name}"
-    network_interface_ids = ["${azurerm_network_interface.incident_response_nic_01.id}"]
+    resource_group_name   = "${azurerm_resource_group.IR_group.name}"
+    network_interface_ids = ["${azurerm_network_interface.IR_nic_01.id}"]
     vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
@@ -299,7 +299,7 @@ resource "azurerm_virtual_machine" "myterraformvmWIN2K16A" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.incident_response_storage_account_01.primary_blob_endpoint}"
+        storage_uri = "${azurerm_storage_account.IR_storage_account_01.primary_blob_endpoint}"
     }
 
     tags {
@@ -311,8 +311,8 @@ resource "azurerm_virtual_machine" "myterraformvmWIN2K16A" {
 resource "azurerm_virtual_machine" "myterraformvmWIN2K16B" {
     name                  = "WIN2K16B"
     location              = "northcentralus"
-    resource_group_name   = "${azurerm_resource_group.incident_response_group.name}"
-    network_interface_ids = ["${azurerm_network_interface.incident_response_nic_01.id}"]
+    resource_group_name   = "${azurerm_resource_group.IR_group.name}"
+    network_interface_ids = ["${azurerm_network_interface.IR_nic_01.id}"]
     vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
@@ -336,7 +336,7 @@ resource "azurerm_virtual_machine" "myterraformvmWIN2K16B" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.incident_response_storage_account_01.primary_blob_endpoint}"
+        storage_uri = "${azurerm_storage_account.IR_storage_account_01.primary_blob_endpoint}"
     }
 
     tags {
